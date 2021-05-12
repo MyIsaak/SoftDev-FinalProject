2527444f8075d1397dcc02379a036e554cba1859
Karim Ghosn <ghosnk99@Karim-MacBook-Air.local>
2021-05-13 00:06:32 +0300

    weather and time

--- main.py
+++ main.py
@@ -76,6 +76,24 @@ def verify_login():
     else:
         messagebox.showerror(title="Error", message="No user found")
 
+def update_Amount():
+    conn = sqlite3.connect('Money_Transaction.db')
+    c = conn.cursor()
+    c.execute("SELECT Balance FROM Account")
+    records = c.fetchall()
+    # print(int(''.join(map(str, records[0]))))  # int value
+
+    totalBalancevar = str(''.join(map(str, records[0])))
+
+    conn.commit()
+
+    amount_label['text'] = "Total amount: "+totalBalancevar+" €"
+    amount_label.after(2000, update_Amount)
+    conn.close()
+
+def show_frame(frame):
+    frame.tkraise()
+
 def open_mainwindow():
     global clockBtn
     global totalBalancevar
@@ -111,10 +129,39 @@ def open_mainwindow():
     Help_menu = Menu(MyOwnMenu)
     MyOwnMenu.add_cascade(label="File", menu=Help_menu)
 
+    # Frames:
 
+    frame_add = Frame(mainWindow, width=280, height=480,)
 
-# functions
+    frame_add.grid(row=0, column=0, padx=10, pady=10, sticky='nw')
+    frame_add.grid_propagate(False)
 
+    frame_middle_1 = Frame(mainWindow, width=590, height=480,)
+
+    frame_middle_2 = Frame(mainWindow, width=590, height=480,)
+    frame_middle_3 = Frame(mainWindow, width=590, height=480,)
+
+    for frame in (frame_middle_1, frame_middle_2, frame_middle_3):
+        frame.grid(row=0, column=1, padx=10, pady=10, sticky='nw')
+        frame.grid_propagate(False)
+
+    show_frame(frame_middle_1)
+
+    api = "https://api.openweathermap.org/data/2.5/weather?q=" + \
+        "Beirut"+"&appid=0ba604883a3d0c62f8151ad357a2f74d"
+
+    json_data = requests.get(api).json()
+    condition = json_data['weather'][0]['main']
+    temp = int(json_data['main']['temp'] - 273.15)
+
+    final_info = condition + " Beirut " + str(temp) + "°C"
+
+    # final_info = "NO INTERNET CONNECTION"
+
+    weatherLabel = Label(
+        frame_middle_1, text="weather "+final_info+"", borderwidth=1, relief="solid", font=('Arial Bold', 10))
+    weatherLabel.grid(
+        row=0, column=0, pady=(0, 20), padx=20, ipadx=10, ipady=10,  sticky='ew')
 
 # Buttons
     LogoutButton = Button(frame_middle_1, text="Logout", bg="Blue", fg="white", height=1, width=15, font="Raleway", command=mainWindow.quit)
@@ -139,6 +186,26 @@ def open_mainwindow():
     GetTimeButton = Button(frame_add, image=ClockButton, border=0, command=time)
 
     GetTimeButton.grid(row=0, column=0, pady=20, padx=5, sticky="ew")
+
+
+
+# Time Label
+    
+    global time_label
+    time_label = Label(frame_add, text="Pick Time", font=("Arial", 10))
+    time_label.grid(row=1, column=0, pady=5, padx=20,)
+
+    global amount_label
+    amount_label = Label(frame_middle_1, borderwidth=1, relief="solid", font=('Arial Bold', 10))
+    amount_label.grid(row=1, column=0, pady=(0, 20), padx=20, ipadx=10, ipady=10,  sticky='ew')
+
+    # Calender
+
+    cal = Calendar(frame_add, selectmode="day", year=2021, month=4, day=27, background="Blue", textvariable=datevar)
+    cal.grid(row=2, column=0, pady=20, padx=20, )
+    update_Amount()
+    conn.close()
+
 
 
     # Buttons
